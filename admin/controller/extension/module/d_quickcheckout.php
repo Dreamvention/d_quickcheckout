fc159d748b5b2adbb27994ed7e600b6c74c4764b
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-11-08 17:36:00 +0200

    fix

--- admin/controller/extension/module/d_quickcheckout.php
+++ admin/controller/extension/module/d_quickcheckout.php
@@ -325,7 +325,7 @@ class ControllerExtensionModuleDQuickcheckout extends Controller {
             $this->cart->add($product['product_id']);
         }
         $this->load->model('extension/d_opencart_patch/user');
-        $data['editor'] = $url.'index.php?route=checkout/checkout&edit&setting_id='.$setting_id.'&admin='.urlencode($admin.'?'.$this->model_extension_d_opencart_patch_user->getUrlToken().'&custom_field='. urlencode($custom_field.'?'.$this->model_extension_d_opencart_patch_user->getUrlToken());
+        $data['editor'] = $url.'index.php?route=checkout/checkout&edit&setting_id='.$setting_id.'&admin='.urlencode($admin.'?'.$this->model_extension_d_opencart_patch_user->getUrlToken()).'&custom_field='. urlencode($custom_field .'?'. $this->model_extension_d_opencart_patch_user->getUrlToken());
         $this->response->setOutput($this->model_extension_d_opencart_patch_load->view('extension/d_quickcheckout/editor', $data));
     }
 

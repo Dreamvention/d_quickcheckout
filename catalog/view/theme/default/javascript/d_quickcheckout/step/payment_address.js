eb5b1245a9fac5acaa94c5d2c3285de41df9dd28
artem <php6.drvn@gmail.com>
2019-08-15 11:44:25 +0300

    Added 'file' custom field

--- catalog/view/theme/default/javascript/d_quickcheckout/step/payment_address.js
+++ catalog/view/theme/default/javascript/d_quickcheckout/step/payment_address.js
@@ -30,5 +30,43 @@
         this.setState(data);
     });
 
+    this.subscribe('payment_address/upload_file', function(data) {
+        
+        $.ajax({
+            url: 'index.php?route=tool/upload',
+            type: 'post',
+            dataType: 'json',
+            data: data.FormData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $('#button-payment-custom-field'+ data.id ).button('loading');
+            },
+            complete: function() {
+                $('#button-payment-custom-field'+ data.id ).button('reset');
+            },
+            success: function(json) {
+                $('#button-payment-custom-field'+ data.id ).parent().find('.text-danger').remove();
+
+                if (json['error']) {
+                    $('#button-payment-custom-field'+ data.id ).parent().find($('#'+data.step+data.field)).after('<div class="text-danger">' + json['error'] + '</div>');
+                }
+
+                if (json['success']) {
+                    alert(json['success']);
+
+                    $('#button-payment-custom-field'+ data.id ).parent().find($('#'+data.step+data.field)).val(json['code']);
+                    var state =  { 'payment_address': {} } 
+                    state['payment_address'][data.field] = json['code'];
+                
+                    this.dispatch('payment_address/update', state);
+                }
+            }.bind(this),
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+        });
+    });
 
 })(qc);
